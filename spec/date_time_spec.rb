4b324d595de5bf9701565c6e1a602a8ccd9d0bbd
Hiro Asari <asari.ruby@gmail.com>
2012-09-28 19:54:46 -0400

    Basic passing specs for DateTime extensions for the Hanke-Henry calendar.

--- spec/date_time_spec.rb
+++ spec/date_time_spec.rb
@@ -3,14 +3,21 @@ require 'hanke-henry-calendar'
 
 describe 'DateTime' do
   describe '.hh' do
-    context 'when passed 2012, 1, 2, 3, 4, 5' do
+    context 'when passed 2012, 1, 1, 0, 0, 0' do
       before :each do
-        @datetime = DateTime.hh(2012, 1, 2, 3, 4, 5)
+        @datetime = DateTime.hh(2012, 1, 1, 0, 0, 0)
       end
     
       it 'creates a DateTime object' do
         @datetime.should be_true
         @datetime.should be_a DateTime
+      end
+      
+      describe '#jd' do
+        let(:subject) { @datetime.jd }
+        it 'returns the same value as DateTime.new(2012,1,1,0,0,0).jd' do
+          subject.should == DateTime.new(2012,1,1,0,0,0).jd
+        end
       end
     end
     
@@ -44,6 +51,37 @@ describe 'DateTime' do
     
   end
   
+  # Jan 1, 2012 designate the same day on both Gregorian and Hanke-Henry calendards
+  describe '#hh' do
+    context 'when called on DateTime.new(2012,1,1)' do
+      let(:subject) { DateTime.new(2012,1,1) }
+      it 'returns the same value as the Greogrian calendar counterpart' do
+        subject.hh.should == '2012-1-1'
+      end
+    end
+  end
+  
+  # Jan 1, 2400 on the Gregorian = Dec 31, 2399 on H-H
+  describe '#hh' do
+    context 'when called on DateTime.new(2400,1,1)' do
+      let(:subject) { DateTime.new(2400,1,1) }
+      it 'returns "2399-12-31"' do
+        subject.hh.should == '2399-12-31'
+      end
+    end
+  end
+  
+  # http://henry.pha.jhu.edu/calendarDir/ccctDir/ccctHTML/2015.1.html
+  # Dec 31, 2015 on Gregorian = X 5, 2015
+  describe '#hh' do
+    context 'when called on DateTime.new(2015,12,31)' do
+      let(:subject) { DateTime.new(2015,12,31) }
+      it 'returns "2015-x-5"' do
+        subject.hh.should == '2015-x-5'
+      end
+    end
+  end
+
   describe '#xtr?' do
     context 'when called on a normal DateTime object with year 2012' do
       let(:subject) { DateTime.new(2012) }
@@ -52,20 +90,10 @@ describe 'DateTime' do
       end
     end
     
-    context 'when called on a DateTime object instantiated by .hh(2012)' do
-      let(:subject) { DateTime.hh(2012) }
-      it 'returns false' do
-        subject.xtr?.should be_false
-      end
-    end
-
     context 'when called on a DateTime object for year 2015' do
-      before :each do
-        @datetime = DateTime.hh(2015)
-      end
-      it 'returns false' do
-        @datetime.hh_year.should == 2015
-        @datetime.xtr?.should be_true
+      let(:subject) { DateTime.hh(2015) }
+      it 'returns true' do
+        subject.xtr?.should be_true
       end
     end
   end
